de43f64689732089fe8897e16400ca7ba5b853ff
zplato <zach.plato@gmail.com>
2025-05-29 23:37:27 -0400

    1768. Resolved with Alt. Solution

--- Easy/1768. Merge Strings Alternately/1768. Merge Strings Alternately.py
+++ Easy/1768. Merge Strings Alternately/1768. Merge Strings Alternately.py
@@ -11,18 +11,29 @@ class Solution:
 
         # Approach 1 - use two pointers for each word and iterate through a loop until both words are finished
 
-        i, j = 0, 0
-        result = []  # Using a list here instead of a string
-        # Adding to the end of a string rebuilds it each time
-        # which causes O(n^2 behavior)
+        # i, j = 0, 0
+        # result = []  # Using a list here instead of a string
+        # # Adding to the end of a string rebuilds it each time
+        # # which causes O(n^2 behavior)
+        #
+        # while i < len(word1) or j < len(word2):
+        #     if i < len(word1):
+        #         result.append(word1[i])
+        #         i += 1
+        #     if j < len(word2):
+        #         result.append(word2[j])
+        #         j += 1
+        #
+        # return ''.join(result)
 
-        while i < len(word1) or j < len(word2):
+        # Approach 2 - Utilize a single pointer and iterate over the words
+        n = max(len(word1), len(word2))
+        result = []  # utilize a list here as appending to a string causes O(n^2) behavior due to rebuilding the string
+        for i in range(n):
             if i < len(word1):
                 result.append(word1[i])
-                i += 1
-            if j < len(word2):
-                result.append(word2[j])
-                j += 1
+            if i < len(word2):
+                result.append(word2[i])
 
         return ''.join(result)
 
@@ -55,10 +66,11 @@ if __name__ == "__main__":
 
 
 """
-Time Complexity: O(n + m)
-    - Where n and m are lengths of word1 and word2
-    - One pass through each word
-
-Space Complexity: O(n + m)
-    - Building result list of total combined size
+For both approaches 1. and 2. - 
+    Time Complexity: O(n + m)
+        - Where n and m are lengths of word1 and word2
+        - One pass through each word
+    
+    Space Complexity: O(n + m)
+        - Building result list of total combined size
 """
