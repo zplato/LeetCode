b5883b85507905fbdbb4c9a8c74d60a5a9bc4b35
zplato <zach.plato@gmail.com>
2025-06-05 20:21:45 -0400

    Add DevOps OOP example

--- DevOpsUtils/OOP_practice.py
+++ DevOpsUtils/OOP_practice.py
@@ -19,11 +19,11 @@ class BankAccount(ABC):
 class CheckingAccount(BankAccount):
     def __init__(self, balance, overdraft_fee):
         super().__init__(balance)
-        self.__overdraft_fee = overdraft_fee
+        self._overdraft_fee = overdraft_fee
 
     def withdraw(self, amount):
         if amount > self._balance:
-            self._balance -= (amount + self.__overdraft_fee)
+            self._balance -= (amount + self._overdraft_fee)
         else:
             self._balance -= amount
 
@@ -33,13 +33,30 @@ class CheckingAccount(BankAccount):
 class SavingsAccount(BankAccount):
     def __init__(self, balance):
         super().__init__(balance)
-        self.__interest_rate = 0.10
+        self._interest_rate = 0.01
 
     def apply_interest(self):
-        self._balance += (self._balance * self.__interest_rate)
+        self._balance += (self._balance * self._interest_rate)
 
     def get_balance(self):
         print("Current Savings Account Balance: {0}".format(self._balance))
+
+class HighYieldSavingsAccount(SavingsAccount):
+    def __init__(self, balance):
+        super().__init__(balance)
+        self._interest_rate = 0.1
+        self._minimum_qualified_amount = 1000
+        self._qualified = self._check_qualifications()
+
+    def _check_qualifications(self):
+        if self._balance > self._minimum_qualified_amount:
+            print("Balance Qualified!")
+            return True
+        else:
+            print(f"Balance doesn't qualify, need at least {self._minimum_qualified_amount}")
+            return False
+
+
 
 def main():
     my_savings = SavingsAccount(balance=100)
@@ -54,6 +71,12 @@ def main():
     my_checking.get_balance()
     my_checking.withdraw(1100)
     my_checking.get_balance()
+
+    # High Yield Savings
+    my_hysa = HighYieldSavingsAccount(balance=999)
+    my_hysa.apply_interest()
+    my_hysa.deposit(1)
+    my_hysa.apply_interest()
 
 
 if __name__ == "__main__":
